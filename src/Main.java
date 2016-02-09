cf5ade752514225c9e08d1752155bcc1f29a6802
Liam Kreiss <liam.kreiss@gmail.com>
2016-02-09 12:09:53 -0800

    made some beginning work

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
-
+	public static void main(String[] args){
+		Scanner console = new Scanner(System.in);
+		System.out.println("This program matches potential lovebirds. Please follow the instructions.");
+		System.out.println("Type \"q\" at any time to quit.");
+		System.out.print("Name: ");
+		String name = console.nextLine().toLowerCase();
+		while (!name.equals("q")){
+			
+		}
+	}
 }
