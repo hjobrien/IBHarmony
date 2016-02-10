ab01f7bbe2a6891aedf5452ff56960081bda5a1c
Liam Kreiss <liam.kreiss@gmail.com>
2016-02-10 09:36:27 -0800

    an idea of how we could easily input info

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException{
 		Scanner console = new Scanner(System.in);
 		System.out.println("This program matches potential lovebirds. Please follow the instructions.");
 		System.out.println("Type \"q\" at any time to quit.");
-		System.out.print("Name: ");
-		String name = console.nextLine().toLowerCase();
-		while (!name.equals("q")){
-			
+		PrintStream p = new PrintStream(new File("PersonalData.txt"));
+		boolean cont = true;
+		do {
+			cont = ask("Name: ", console, p);
+			if (cont){
+				cont = ask("Grade: ", console, p);
+				if (cont){
+					cont = ask("Gender: ", console, p);
+					if (cont){
+						cont = ask("Preferred Gender: ", console, p);
+						if (cont){
+							askQuestions(console, p);
+						}
+					}
+				}
+			}
+			System.out.println();
+			p.println();
+		} while (cont == true);
+	}
+
+	private static void askQuestions(Scanner console, PrintStream p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static boolean ask(String s, Scanner console, PrintStream p) {
+		System.out.print(s);
+		String answer = console.nextLine();
+		if (answer.toLowerCase().equals("q")){
+			return false;
 		}
+		p.print(answer + " ");
+		return true;
 	}
 }
