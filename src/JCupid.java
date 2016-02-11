0b8a59a5a80b53fd69ac8023835077910196b8d0
Liam Kreiss <liam.kreiss@gmail.com>
2016-02-11 12:15:52 -0800

    made Jcupid work really well

--- src/JCupid.java
+++ src/JCupid.java
@@ -1,54 +1,105 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JCupid {
 	
-	public static final int NUM_QUESTIONS = 9;
-	public static final int[] WEIGHTS = new int[]{1,1,1,1,1,1,1,1,1};
+	public static final int NUM_QUESTIONS = 14;
+	public static final int[] WEIGHTS = new int[]{3,2,1,1,1,1,1,3,3,1,2,2,1,1};
 	
-	public static final String fileName = "PersonalData.txt";
+	public static final String fileName = "FullInfo.txt";
 	
 	public static void main(String[] args){
-		compare(fileName);
+		jCupid(fileToList());
 	}
 	
-	public static void compare(String fileName){
+	public static ArrayList<Person> fileToList(){
+		ArrayList<Person> people = new ArrayList<Person>();
 		Scanner fileReader = null;
 		try {
 			fileReader = new Scanner(new File(fileName));
-		} catch (FileNotFoundException e) {}
-		
-		
-		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		while(fileReader.hasNextLine()){
-			Scanner fileReader2 = null;
-			try {
-				fileReader2 = new Scanner(new File(fileName));
-			} catch (FileNotFoundException e) {}
-			String[] p1Data = fileReader.nextLine().split(" ");
-			String p1Answers = p1Data[4];
-			String bestMatch = p1Data[0];
+			String name = fileReader.nextLine();
+			int grade = fileReader.nextInt();
+			char gender = fileReader.next().toLowerCase().charAt(0);
+			char genderPreference = fileReader.next().toLowerCase().charAt(0);
+			String answers = fileReader.next();
+			people.add(new Person(name, grade, gender, genderPreference, answers));
+			if (fileReader.hasNextLine()){
+				fileReader.nextLine();
+			}
+		}
+		return people;
+	}
+	
+	public static void jCupid(ArrayList<Person> eligibleCandidates){
+//		//this scanner takes each name in order from the file, this person is the person to be matched with someone else
+//		Scanner fileReader = null;
+//		try {
+//			fileReader = new Scanner(new File("PersonalData.txt"));
+//		} catch (FileNotFoundException e) {}
+		
+		
+		
+//		while(fileReader.hasNextLine()){
+		for(int i = 0; i < eligibleCandidates.size(); i++){
+//			//this scanner is reset for each name generated y the first one, it also gets each name from the file as a candidate for matching
+//			Scanner fileReader2 = null;
+//			try {
+//				fileReader2 = new Scanner(new File("PersonalData.txt"));
+//			} catch (FileNotFoundException e) {}
+//			String[] p1Data = fileReader.nextLine().split(" ");
+			//the series of 1s or 2s that represent the answers to the questions
+			Person p1 = eligibleCandidates.get(i);
+			String p1Answers = p1.getAnswers();
+//			String bestMatch = eligibleCandidates.get(i).getName();
+			Person bestMatch = eligibleCandidates.get(i);
 			double goodFitCount = 0;
 			double max = -1;
 			
-			while(fileReader2.hasNextLine()){
+			for(int j = 0; j < eligibleCandidates.size(); j++){
+			Person p2 = eligibleCandidates.get(j);
+
+//			while(fileReader2.hasNextLine()){
 				goodFitCount = 0;
-				String[] p2Data = fileReader2.nextLine().split(" ");
-				if(p2Data[1].equals(p1Data[1]) && p2Data[2].equals(p1Data[3]) && p2Data[3].equals(p1Data[2])){
-					String p2Answers = p2Data[4];
-					for(int i = 0; i < NUM_QUESTIONS; i++){
-						if(p1Answers.charAt(i) == p2Answers.charAt(i)){
-							goodFitCount+=WEIGHTS[i];
+//				String[] p2Data = fileReader2.nextLine().split(" ");
+				/*checks for:
+				 * different names
+				 * same grade
+				 * if preferences line up (m,f -> f,m)
+				 */
+				String p1Name = p1.getName();
+				String p2Name = p2.getName();
+				int p1Grade = p1.getGrade();
+				int p2Grade = p2.getGrade();
+				char p1Gender = p1.getGender();
+				char p2Gender = p2.getGender();
+				char p1GenderPref = p1.getGenderPreference();
+				char p2GenderPref = p2.getGenderPreference();
+
+
+				if((!p1.isMatched() || !p2.isMatched()) && !p1Name.equals(p2Name) && p1Grade == p2Grade && p1Gender == p2GenderPref && p2Gender == p1GenderPref){
+					String p2Answers = p2.getAnswers();
+					for(int k = 0; k < NUM_QUESTIONS; k++){
+						if(p1Answers.charAt(k) == p2Answers.charAt(k)){
+							//increments the similarity index by the weight for each question (change weights at the top)
+							goodFitCount+=WEIGHTS[k];
 						}
 					}
 					if(goodFitCount > max){
-						bestMatch = p2Data[0];
+						bestMatch = new Person(p2Name, p2Grade, p2Gender, p2GenderPref, p2Answers);
 						max = goodFitCount;
 					}
 				}
 			}
-			System.out.println(p1Data[0] + " " + bestMatch);
+			bestMatch.hasBeenMatched();
+			p1.hasBeenMatched();
+			System.out.println("\"" + p1.getName() + "\"-\"" + bestMatch.getName() + "\"");
 			
 		}
 	}
