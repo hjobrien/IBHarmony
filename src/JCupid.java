c0cf2555ffc11b2fc2cfd4e9154e54a81e091b6d
Liam Kreiss <liam.kreiss@gmail.com>
2016-02-11 21:45:45 -0800

    added options for processing and generating files

--- src/JCupid.java
+++ src/JCupid.java
@@ -8,14 +8,15 @@ public class JCupid {
 	//each number is the weight of that question, 1 is average (less important questions should have weight < 1, more important ones should be > 1
 	public static final int[] WEIGHTS = new int[]{3,2,1,1,1,1,1,3,3,1,2,2,1,1};
 	
-	public static final String fileName = "PersonalData.txt";
+	public static final String generatedFileName = "PersonalData.txt";
+	public static final String processedFileName = "PersonalData.txt";
 	
 	public static void main(String[] args) throws FileNotFoundException{
 		//if you want to generate a file yourself
-		FileGenerator fg = new FileGenerator(NUM_QUESTIONS, fileName);
+		FileGenerator fg = new FileGenerator(NUM_QUESTIONS, generatedFileName);
 		
 		//creates the class that matches people together
-		MatchPeople mp = new MatchPeople(fileName, WEIGHTS, NUM_QUESTIONS);
+		MatchPeople mp = new MatchPeople(processedFileName, WEIGHTS, NUM_QUESTIONS);
 		
 		//actually does the matching and processing
 		mp.jCupid(mp.fileToList());
