ae28d5576327604e72866ee19302d53b7aae489e
Liam Kreiss <liam.kreiss@gmail.com>
2016-02-12 08:54:10 -0800

    added question

--- src/JCupid.java
+++ src/JCupid.java
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class JCupid {
 	
@@ -10,11 +11,16 @@ public class JCupid {
 //	public static final int[] WEIGHTS = new int[]{8,7,5,6,1,2,1,9,10,1,4,3,1,1};
 	
 	public static final String GENERATED_FILE_NAME = "PersonalData.txt";
-	public static final String PROCESSED_FILE_NAME = "FullData.txt";
+	public static final String PROCESSED_FILE_NAME = "PersonalData.txt";
 	
 	public static void main(String[] args) throws FileNotFoundException{
+		
 		//if you want to generate a file yourself
-//		FileGenerator.genFile(NUM_QUESTIONS, GENERATED_FILE_NAME);
+		Scanner console = new Scanner(System.in);
+		System.out.print("Would you like to enter data? (y for yes) ");
+		if (console.nextLine().toLowerCase().equals("y")){
+			FileGenerator.genFile(NUM_QUESTIONS, GENERATED_FILE_NAME);
+		}
 		
 		//creates the class that matches people together
 		MatchPeople mp = new MatchPeople(PROCESSED_FILE_NAME, WEIGHTS, NUM_QUESTIONS);
