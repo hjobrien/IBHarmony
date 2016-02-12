1523bbc3ea2f93e9446c83e83b3e830ff1b42347
Liam Kreiss <liam.kreiss@gmail.com>
2016-02-11 21:37:30 -0800

    minor fixes

--- src/JCupid.java
+++ src/JCupid.java
@@ -1,19 +1,23 @@
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.util.Scanner;
 
 public class JCupid {
+	
 	//these values can be changed to reflect the quiz
 	public static final int NUM_QUESTIONS = 14;
+	
 	//each number is the weight of that question, 1 is average (less important questions should have weight < 1, more important ones should be > 1
 	public static final int[] WEIGHTS = new int[]{3,2,1,1,1,1,1,3,3,1,2,2,1,1};
 	
 	public static final String fileName = "PersonalData.txt";
 	
 	public static void main(String[] args) throws FileNotFoundException{
+		//if you want to generate a file yourself
 		FileGenerator fg = new FileGenerator(NUM_QUESTIONS, fileName);
+		
+		//creates the class that matches people together
 		MatchPeople mp = new MatchPeople(fileName, WEIGHTS, NUM_QUESTIONS);
+		
+		//actually does the matching and processing
 		mp.jCupid(mp.fileToList());
 	}
 }
